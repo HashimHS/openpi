3119489793dcc84d7d330b32c4b4b44113aff1c0
hashim.ismail <hashim.ismail@cs.lth.se>
2026-05-11 14:56:44 +0200

    policy fixes

--- src/openpi/policies/policy_config.py
+++ src/openpi/policies/policy_config.py
@@ -65,7 +65,8 @@ def create_trained_policy(
             raise ValueError("Asset id is required to load norm stats.")
         # print(f"!!!!{data_config.asset_id}")
         # print(robotwin_repo_id)
-        data_config.asset_id = robotwin_repo_id
+        # data_config.asset_id = robotwin_repo_id
+        data_config.asset_id = str(checkpoint_dir).split("/")[-2]
         norm_stats = _checkpoints.load_norm_stats(checkpoint_dir / "assets", data_config.asset_id)
 
     return _policy.Policy(
